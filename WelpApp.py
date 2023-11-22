7e126f538d2861d60881fc25cb38fe44fbed7b34
lawrence leymarie <lawrence.leymarie@gmail.com>
2023-11-21 23:03:16 -0500

    Creates db_operations.py connecting app to mysql server

---
+++ WelpApp.py
@@ -0,0 +1,5 @@
+from db_operations import db_operations
+
+
+if __name__ == "__main__":
+    db_ops = db_operations('sql3664132')
---
+++ db_operations.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+# class responsible for database operations
+class db_operations():
+    def __init__(self,dbName, user = 'sql3664132', password = 'MpTl3GKGRq', host = 'sql3.freemysqlhosting.net' ):
+        config = {
+            'user': user,
+            'password': password,
+            'host': host,
+            'database': dbName,
+            'raise_on_warnings': True
+            }
+        try:
+            self.connection = mysql.connector.connect(**config)
+            self.cursor = self.connection.cursor()
+            print("success")
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+
+
+    def destructor(self):
+        if self.connection.is_connected():
+                self.cursor.close()
+                self.connection.close()
+                print("MySQL connection is closed")
+    
+    def PrintDB(self):
+        self.cursor.execute("SHOW TABLES")
+        tables = self.cursor.fetchall()
+
+        for table_name in tables:
+            print(f"Contents of table {table_name[0]}:")
+
+            # Querying each table
+            self.cursor.execute(f"SELECT * FROM {table_name[0]}")
+            rows = self.cursor.fetchall()
+
+            # Printing the contents of each table
+            for row in rows:
+                print(row)
+            print("\n")
+    
